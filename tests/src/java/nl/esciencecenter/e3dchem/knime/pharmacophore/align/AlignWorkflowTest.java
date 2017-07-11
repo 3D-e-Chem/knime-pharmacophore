059824e33d4acd0672ec472c9d498ea66a90e3fe
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2017-07-11 08:19:41 +0200

    Added align workflow test

---
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/pharmacophore/align/AlignWorkflowTest.java
@@ -0,0 +1,36 @@
+package nl.esciencecenter.e3dchem.knime.pharmacophore.align;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.workflow.UnsupportedWorkflowVersionException;
+import org.knime.core.util.LockFailedException;
+import org.knime.testing.core.TestrunConfiguration;
+
+import nl.esciencecenter.e3dchem.knime.testing.TestFlowRunner;
+
+public class AlignWorkflowTest {
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+	private TestFlowRunner runner;
+
+	@Before
+	public void setUp() {
+		TestrunConfiguration runConfiguration = new TestrunConfiguration();
+		runConfiguration.setLoadSaveLoad(false);
+		runner = new TestFlowRunner(collector, runConfiguration);
+	}
+
+	@Test
+	public void test_read_and_write() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		File workflowDir = new File("src/knime/align-test");
+		runner.runTestWorkflow(workflowDir);
+	}
+}
