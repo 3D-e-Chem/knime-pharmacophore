0b9e2a5100f034c7dd22170b94fe17543c95f968
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2018-07-05 14:08:51 +0200

    Remove unused import

--- tests/src/java/nl/esciencecenter/e3dchem/knime/pharmacophore/PharmacophorePointTest.java
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/pharmacophore/PharmacophorePointTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Locale;
-
 import org.ejml.simple.SimpleMatrix;
 import org.junit.Test;
 
